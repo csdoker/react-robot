026d59cd43b293b7cea3447fdc861f643fe04619
csdoker <csd758371536@qq.com>
2021-02-12 20:14:48 +0800

    feat: 完成列表的渲染

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+import robots from './mock/robots.json'
+import Robot from './components/Robot'
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {robots.map(robot => <Robot id={robot.id} email={robot.email} name={robot.name} />)}
+    </ul>
+  )
 }
 
-export default App;
+export default App
