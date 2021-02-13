a9d90b1fcf32255ab7daf157d0a8d2513b121465
csdoker <csd758371536@qq.com>
2021-02-13 15:01:59 +0800

    feat: 创建class类组件

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styles from './App.module.css'
 import logo from './assets/images/logo.svg'
 import robots from './mock/robots.json'
 import Robot from './components/Robot'
+import ShoppingCart from './components/ShoppingCart'
 
 function App () {
   return (
@@ -11,6 +12,7 @@ function App () {
         <img className={styles.appLogo} src={logo} alt='' />
         <h1>机器人购物平台</h1>
       </div>
+      <ShoppingCart />
       <div className={styles.robotList}>
         {robots.map(robot => <Robot id={robot.id} email={robot.email} name={robot.name} />)}
       </div>
