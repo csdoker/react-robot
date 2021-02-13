a876fef6af4eb455f5a829e59b75ebb9488c7e46
csdoker <csd758371536@qq.com>
2021-02-13 16:11:27 +0800

    feat: 添加事件处理逻辑

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import styles from './ShoppingCart.module.css'
 
+import { FiShoppingCart } from 'react-icons/fi'
+
 interface Props {
 
 }
@@ -17,10 +19,19 @@ class ShoppingCart extends Component<Props, State> {
     }
   }
 
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if ((e.target as HTMLElement).nodeName === 'SPAN') {
+      this.setState({ isOpen: !this.state.isOpen })
+    }
+  }
+
   render () {
     return (
       <div className={styles.cartContainer}>
-        <button className={styles.button} onClick={() => { this.setState({ isOpen: !this.state.isOpen }) }}>购物车：2（件）</button>
+        <button className={styles.button} onClick={this.handleClick}>
+          <FiShoppingCart />
+          <span>购物车：2（件）</span>
+        </button>
         <div className={styles.cartDropDown} style={{display: this.state.isOpen ? 'block' : 'none'}}>
           <ul>
             <li>robot 1</li>
