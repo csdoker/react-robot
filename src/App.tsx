87c8f56d2d4fda0647ee26dbdbbc713cc41f5b25
csdoker <csd758371536@qq.com>
2021-02-13 14:44:35 +0800

    feat: 加载媒体与字体文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './App.module.css'
-
+import logo from './assets/images/logo.svg'
 import robots from './mock/robots.json'
 import Robot from './components/Robot'
 
 function App () {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img className={styles.appLogo} src={logo} alt='' />
+        <h1>机器人购物平台</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map(robot => <Robot id={robot.id} email={robot.email} name={robot.name} />)}
       </div>
